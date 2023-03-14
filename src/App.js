5c9853d647562347675fa71b3b6a41292eb65288
will <thewillboesetrio@gmail.com>
2023-03-14 10:52:00 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+
+  const nameInputRef = useRef();
+  const emailInputRef = useRef();
+  const zipInputRef = useRef();
+
+  let [nameError, setNameError] = useState(false);
+  let [emailError, setEmailError] = useState(false);
+  let [zipError, setZipError] = useState(false);
+  let [formSubmitted, setFormSubmitted] = useState(false);
+
+  function handleSubmit(event) {
+    //event.preventDefault();
+    setFormSubmitted(true);
+
+    const enteredName = nameInputRef.current.value;
+    if(enteredName.length < 3){setNameError(true); event.preventDefault()}else(setNameError(false));
+
+    const enteredEmail = emailInputRef.current.value;
+   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(enteredEmail)){
+    setEmailError(false)
+   }else{
+    setEmailError(true);
+    event.preventDefault();
+   }
+
+   const enteredZip = zipInputRef.current.value;
+   if (/(^\d{5}$)|(^\d{5}-\d{4}$)/.test(enteredZip)){
+    setZipError(false)
+   }else{
+    setZipError(true);
+    event.preventDefault();
+   }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Form Validation</h2>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input type="text" id="name" ref={nameInputRef} required></input>
+            {nameError && 
+            <p className='error'>NAME MUST BE 3 CHARACTERS OR MORE</p>
+            }
+            </div>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input type="text" id="email" ref={emailInputRef} required></input>
+            {emailError && 
+            <p className='error'>MUST BE VALID EMAIL</p>
+            }
+          </div>
+          <div>
+            <label htmlFor='zip'>Zip Code</label>
+            <input type="text" id="zip" ref={zipInputRef} required></input>
+            {zipError && 
+            <p className='error'>MUST BE VALID ZIP CODE</p>
+            }
+          </div>
+          <div>
+            <button>SUBMIT</button>
+          </div>
+        </form>
+        <div>
+        {!nameError && !emailError && !zipError && formSubmitted &&
+            <p className='success'>FORM SUBMIT SUCCESSFUL</p>
+            }
+        </div>
+      </div>
     </div>
   );
 }
